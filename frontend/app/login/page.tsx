8963a136eb5a238151308030d019a783b20d254c
Charan Gutti <charan.gutti@gmail.com>
2024-09-12 19:56:38 +0530

    Reverting changes

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,7 +3,7 @@ import CustomButton from "@/components/CustomButton";
 export default function Login() {
   return (
     <div className="p-10">
-      <CustomButton>Login 2</CustomButton>
+      <CustomButton>Login</CustomButton>
       {/* <button>Click me</button> */}
     </div>
   );
