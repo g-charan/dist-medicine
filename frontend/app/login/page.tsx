d5eb59af4d4d93b9e078bbd30e3e3d029a2a5739
Charan Gutti <charan.gutti@gmail.com>
2024-09-12 19:55:57 +0530

    Initial commit to branch

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,7 +3,7 @@ import CustomButton from "@/components/CustomButton";
 export default function Login() {
   return (
     <div className="p-10">
-      <CustomButton>Login</CustomButton>
+      <CustomButton>Login 2</CustomButton>
       {/* <button>Click me</button> */}
     </div>
   );
