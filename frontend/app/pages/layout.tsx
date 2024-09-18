9a6900da94d18a88096a7519c3a019ba9073d56d
Charan Gutti <charan.gutti@gmail.com>
2024-09-18 13:06:09 +0530

    New Changes 18092024

--- frontend/app/pages/layout.tsx
+++ frontend/app/pages/layout.tsx
@@ -1,72 +1,9 @@
 "use client";
 
-import Sidebar from "@/components/Sidebar";
-import TopNavbar from "@/components/TopNavbar";
-import Cal from "../assets/Calendar.png";
-import Chart from "../assets/Chart.png";
-import Chart_fill from "../assets/Chart_fill.png";
-import Chat from "../assets/Chat.png";
-import Search from "../assets/Search.png";
-import user from "../assets/User.png";
-
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const MENUS = [
-    { title: "Dashboard", src: Chart_fill },
-    {
-      title: "Inventory",
-      src: Chat,
-      submenu: [
-        { title: "View inventory", route: "/pages/hospital/Inventory" },
-        { title: "Inventory Statistics", route: "/pages/hospital/Inventory" },
-      ],
-    },
-    {
-      title: "Orders",
-      src: user,
-      submenu: [
-        { title: "View Orders", route: "/pages/hospital/Orders" },
-        { title: "Request New Order", route: "/pages/hospital/Inventory" },
-        { title: "Orders status", route: "/pages/hospital/Inventory" },
-        { title: "Track your order", route: "/pages/hospital/Inventory" },
-      ],
-    },
-    {
-      title: "Testings",
-      src: Cal,
-      submenu: [
-        { title: "View Testings", route: "" },
-        { title: "Request New Testing", route: "" },
-        { title: "Testing Status", route: "" },
-        { title: "View Laboratories", route: "" },
-      ],
-    },
-    {
-      title: "Vendors",
-      src: Search,
-      submenu: [
-        { title: "View Vendors", route: "" },
-        { title: "Market place", route: "" },
-        { title: "Feedback", route: "" },
-      ],
-    },
-    { title: "Analytics", src: Chart, route: "/Analytics" },
-  ];
-  return (
-    <div className="flex flex-row w-screen h-screen overflow-hidden">
-      <div className="w-fit">
-        <Sidebar MENUS={MENUS} />
-      </div>
-
-      <div className="w-full h-full">
-        <div className="p-2 h-[2.5rem]">
-          <TopNavbar />
-        </div>
-        <div className="my-4 px-7 w-full h-[92%]">{children}</div>
-      </div>
-    </div>
-  );
+  return <div>{children}</div>;
 }
---
+++ frontend/components/Testing/TestingStatusItem.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Sidebar } from "primereact/sidebar";
+import { useState } from "react";
+import CustomButton from "../CustomButton";
+
+const TestingStatusItem = ({ status }: any) => {
+  const [open, setOpen] = useState<boolean>();
+  return (
+    <div className="flex justify-between border-2 py-14 p-7 border-black rounded-md">
+      <div className="flex gap-5">
+        <p>ID: 1234</p>
+        <div>
+          {status == 0 && (
+            <p className="bg-yellow-400 bg-opacity-50 px-2 rounded-lg">
+              Pending
+            </p>
+          )}
+          {status == 1 && (
+            <p className="bg-red-400 bg-opacity-50 px-2 rounded-lg">Rejected</p>
+          )}
+          {status == 2 && (
+            <p className="bg-green-400 bg-opacity-50 px-2 rounded-lg">
+              Completed
+            </p>
+          )}
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <CustomButton onClick={() => setOpen(true)}>View Details</CustomButton>
+        <CustomButton>Report</CustomButton>
+      </div>
+      <Sidebar
+        visible={open}
+        position="right"
+        onHide={() => setOpen(false)}
+        className="bg-white border-black w-[75%] text-black"
+      >
+        Testing
+      </Sidebar>
+    </div>
+  );
+};
+
+export default TestingStatusItem;
